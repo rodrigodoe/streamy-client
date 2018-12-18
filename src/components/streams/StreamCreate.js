b53f3254537a5b5909d1a95e7b6c195f3b377e00
Rodrigo Carvalho <roda7x@gmail.com>
2018-12-18 09:01:27 -0300

    adicionando pastas do projeto

---
+++ src/components/streams/StreamCreate.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+
+class StreamCreate extends React.Component {
+  renderInput({ input, label, meta }) {
+    console.log(meta);
+    return (
+      <div className="field">
+        <label>{label} </label>
+        <input {...input} />
+        <div>{meta.error}</div>
+      </div>
+    );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form
+        className="ui form"
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
+        <Field name="title" label="Enter Title" component={this.renderInput} />
+        <Field
+          name="description"
+          label="Enter Description"
+          component={this.renderInput}
+        />
+        <button className="ui button  primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = formValues => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  form: "streamCreate",
+  validate
+})(StreamCreate);
