c292e887637c80e3bbf7a8f9d65b22f10a7d617e
Rodrigo Carvalho <roda7x@gmail.com>
2018-12-19 00:30:34 -0300

    adicionando stream reducer e actions

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,26 +1,38 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
-  renderInput({ input, label, meta }) {
-    console.log(meta);
+  renderError = ({ error, touched }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""} `;
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label} </label>
-        <input {...input} />
-        <div>{meta.error}</div>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     );
-  }
+  };
 
-  onSubmit(formValues) {
-    console.log(formValues);
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
   }
 
   render() {
     return (
       <form
-        className="ui form"
+        className="ui form error"
         onSubmit={this.props.handleSubmit(this.onSubmit)}
       >
         <Field name="title" label="Enter Title" component={this.renderInput} />
@@ -49,7 +61,12 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate
 })(StreamCreate);
+
+export default connect(
+  null,
+  { createStream }
+)(formWrapped);
---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,25 @@
+import _ from "lodash";
+import {
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  EDIT_STREAM,
+  CREATE_STREAM,
+  DELETE_STREAM
+} from "../actions/types";
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    default:
+      return state;
+  }
+};
